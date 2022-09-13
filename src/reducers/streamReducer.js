085d351962ea71e0ce5a64dfb48daac211af3710
Yasir <yasirahmed1995@gmail.com>
2022-09-13 17:06:16 +0400

    streamReducer and authReducer modified

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -7,7 +7,7 @@ import {
   EDIT_STREAM,
 } from "../actions/type";
 
-export default (state = {}, action) => {
+const streamReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
       return { ...state, ..._.mapKeys(action.payload, "id") };
@@ -23,3 +23,5 @@ export default (state = {}, action) => {
       return state;
   }
 };
+
+export default streamReducer;
