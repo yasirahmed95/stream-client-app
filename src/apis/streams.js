ad415b39153d35e7b2b2a6afb8fc1420fad352cd
Yasir <yasirahmed1995@gmail.com>
2022-09-24 15:44:55 +0400

    'stream-updated'

--- src/apis/streams.js
+++ src/apis/streams.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://api.jsonbin.io/v3",
+  headers: {
+    "X-Master-Key":
+      "$2b$10$dhOjSBkhMiuP.UB3.OSUqeoBKs5VaHFoJwNWMBKYi6Nwmgz3iOrG.",
+  },
 });
